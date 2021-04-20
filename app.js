f3344c3aac38b59d4814435963be6d6fc7b0dbd6
padmashri15 <padmashri.s@hcl.com>
2021-04-20 17:35:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as THREE from '/libs/three.module.js';
+import * as THREE from '../libs/three.module.js';
 import { GLTFLoader } from '../libs/GLTFLoader.js';
 import { RGBELoader } from '../libs/RGBELoader.js';
 import { ARButton } from '../libs/ARButton.js';

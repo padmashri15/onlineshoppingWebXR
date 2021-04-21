fe69ef2ed1cc54a04f3f62ffb6d8bbdc5595e104
padmashri15 <padmashri.s@hcl.com>
2021-04-21 20:43:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ class App{
 		// Load a glTF resource
 		loader.load(
 			// resource URL
-			`chair${id}.glb`,
+			`assets/chair${id}.glb`,
 			// called when the resource is loaded
 			function ( gltf ) {
 
